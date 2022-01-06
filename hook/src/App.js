1ff7823ea0097dd85e102794c444929f56aa9710
bae-sh <hyeon1293@gmail.com>
2022-01-06 17:58:10 +0900

    useInput

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+const useInput2 = (initialValue, validator) => {
+    const [value2, setValue] = useState(initialValue);
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        let willUpdate = true;
+
+        willUpdate = validator(value);
+        // if (typeof validator === "function") {
+        // }
+        if (willUpdate) {
+            setValue(value);
+        }
+    };
+    return { value: value2, onChange };
+};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const validator = (value) => !value.includes("@");
+    const name = useInput2("Mr.", validator);
+    return (
+        <div>
+            <input placeholder="Name" {...name} />
+        </div>
+    );
 }
 
 export default App;
